ca2078444f1ec6f559966273adb1463609a8d716
alejoquintana <alejoquintana00@gmail.com>
2022-09-26 19:44:23 -0300

    items model, controller and routes

---
+++ app/Http/Controllers/ItemController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ItemController extends Controller
+{
+    //
+    public function get()
+	{
+		return Item::all();
+	}
+
+	public function create(Request $request)
+	{
+		if(!$request->text) return response("Error dato", 500);
+
+		$item = new Item;
+		$item->text = $request->text;
+		$item->save();
+
+	}
+
+	public function update(Request $request)
+	{
+		$item = Item::find($request->id);
+		$field = $request->field;
+		$value = $request->value;
+
+		if(!$item || !$field) return response("Error dato", 500);
+
+		$item->$field = $value;
+		$item->save();
+	}
+
+	public function destroy($id)
+	{
+		if ($id == 'all') {
+			$records = Item::pluck('id')->toArray();
+			$deleted = Item::destroy($records);
+		}
+		else{
+			$item = Item::find($id);
+			$item->delete();
+		}
+	}
+}
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ItemController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -13,10 +15,13 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-
-Route::get('/test', function () {
-    return "test";
+Route::controller(ItemController::class)->group(function () {
+    Route::get('items', 'get')->name('items.get');
+    Route::post('item', 'create')->name('items.create');
+    Route::put('item', 'update')->name('items.update');
+    Route::delete('item/{id}', 'destroy')->name('items.destroy');
 });
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
